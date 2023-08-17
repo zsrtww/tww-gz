740d47ff8f0c77e0cda29f9cccf4d46d275c58c9
EJ125 <51464325+EJ125@users.noreply.github.com>
2023-08-17 01:52:46 -0700

    Finalize and clean initial flag menus

--- source/flags.cpp
+++ source/flags.cpp
@@ -13,16 +13,21 @@ void setEventFlag(uint16_t flag) {
 
 void updateFlags() {
     g_flags[WATCHED_FF2_HELMAROC_CUTSCENE] = dComIfGs_isEventBit(0x2D01);
-    g_flags[HAVE_HURRICANE_SPIN] = dComIfGs_isEventBit(0x0B20);
     g_flags[ENDLESS_NIGHT] = dComIfGs_isEventBit(0x0A02);
-    g_flags[ORCA_INTRO_CUTSCENE] = dComIfGs_isEventBit(0x0640);
-    g_flags[PUPPET_GANON_CUTSCENE] = dComIfGs_isEventBit(0x3B02);
-    g_flags[MEETING_KORL_CUTSCENE] = dComIfGs_isEventBit(0x0F80);
-    g_flags[COURTYARD_CUTSCENE] = dComIfGs_isEventBit(0x3804);
+    g_flags[GENERAL_POST_FF1_KORL_CUTSCENE] = dComIfGs_isEventBit(0x0F80);
     g_flags[LEAVING_OUTSET_CUTSCENE] = dComIfGs_isEventBit(0x2401);
     g_flags[TETRA_SPOILS_BAG_TEXT] = dComIfGs_isEventBit(0x0808);
     g_flags[BARREL_LAUNCH_CUTSCENE] = dComIfGs_isEventBit(0x0801);
     g_flags[ENTER_KORL_FIRST_TIME] = dComIfGs_isEventBit(0x2A08);
     g_flags[POST_FF1_KORL_CUTSCENE] = dComIfGs_isEventBit(0x0F80);
+    g_flags[RAISE_TOTG] = dComIfGs_isEventBit(0x1E40);
+    g_flags[OPEN_DARK_PORTAL] = dComIfGs_isEventBit(0x3D02);
+    g_flags[DESTROY_HYRULE_BARRIER] = dComIfGs_isEventBit(0x2C02);
+    g_flags[LOAD_TOTG_AFTER_T2Z] = dComIfGs_isEventBit(0x1820);
+    g_flags[OPEN_HYRULE3_WARP] = dComIfGs_isEventBit(0x2D08);
+    g_flags[KOOLOO_LIMPAH] = dComIfGs_isEventBit(0x2E08);
+    g_flags[BALLOON_DISCOUNT] = dComIfGs_isEventBit(0x1A10);
+    g_flags[TINGS_DISCOUNT] = dComIfGs_isEventBit(0x1A08);
+    g_flags[HAND_ME_DOWN_TUNER] = dComIfGs_isEventBit(0x1708);
 
 }
---
+++ source/menus/general_flag_menu.cpp
@@ -0,0 +1,80 @@
+#include "menus/flag_menu.h"
+#include "menus/general_flag_menu.h"
+#include "libtww/d/com/d_com_inf_game.h"
+#include "flags.h"
+
+
+#define LINE_NUM 8
+Cursor GeneralFlagMenu::cursor;
+
+
+Line lines[LINE_NUM] = {
+    {"FF2 Helmaroc Cutscene", WATCHED_FF2_HELMAROC_CUTSCENE, "Switches to animation set 2, among other things", true,
+        &g_flags[WATCHED_FF2_HELMAROC_CUTSCENE]},
+    {"Post FF1 KoRL Cutscene", GENERAL_POST_FF1_KORL_CUTSCENE, "Spawns KoRL, sets savewarp to Windfall and FoF to layer 2", true,
+        &g_flags[GENERAL_POST_FF1_KORL_CUTSCENE]},
+    {"Endless Night", ENDLESS_NIGHT, "Activates Endless Night, incompatible with having Nayru's Pearl", true,
+        &g_flags[ENDLESS_NIGHT]},
+    {"Raise TotG", RAISE_TOTG, "Raises Tower of the Gods", true,
+        &g_flags[RAISE_TOTG]},
+    {"Load TotG After T2Z", LOAD_TOTG_AFTER_T2Z, "Sets FF3 Layer and F&I Arrows at M&C", true,
+        &g_flags[LOAD_TOTG_AFTER_T2Z]},
+    {"Hyrule 3 Warp", OPEN_HYRULE3_WARP, "Opens golden warp to Hyrule 3", true,
+        &g_flags[OPEN_HYRULE3_WARP]},
+    {"Hyrule Barrier", DESTROY_HYRULE_BARRIER, "Barrier Skip Skip", true,
+        &g_flags[DESTROY_HYRULE_BARRIER]},
+    {"Dark Portal", OPEN_DARK_PORTAL, "Opens dark portal between FF3 and Ganon's Tower", true,
+        &g_flags[OPEN_DARK_PORTAL]},
+
+
+};
+
+
+void GeneralFlagMenu::draw() {
+    updateFlags();
+
+
+    cursor.setMode(Cursor::MODE_LIST);
+    cursor.move(0, LINE_NUM);
+
+
+    if (GZ_getButtonTrig(GZPad::B)) {
+        GZ_setMenu(GZ_FLAGS_MENU);
+        return;
+    }
+
+
+    if (GZ_getButtonTrig(GZPad::A)) {
+        switch (cursor.y) {
+        case WATCHED_FF2_HELMAROC_CUTSCENE:
+            setEventFlag(0x2D01);
+            break;
+        case GENERAL_POST_FF1_KORL_CUTSCENE:
+            setEventFlag(0x0F80);
+            break;
+        case ENDLESS_NIGHT:
+            setEventFlag(0x0A02);
+            break;
+        case RAISE_TOTG:
+            setEventFlag(0x1E40);
+            break;
+        case LOAD_TOTG_AFTER_T2Z:
+            setEventFlag(0x1820);
+            break;
+        case OPEN_HYRULE3_WARP:
+            setEventFlag(0x2D08);
+            break;
+        case DESTROY_HYRULE_BARRIER:
+            setEventFlag(0x2C02);
+            break;
+        case OPEN_DARK_PORTAL:
+            setEventFlag(0x3D02);
+            break;
+       
+        }
+    }
+
+
+    GZ_drawMenuLines(lines, cursor.y, LINE_NUM);
+
+}
