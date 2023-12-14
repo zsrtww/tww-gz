c4d3925fc478ec8b12472aa6867305a380c6009b
EJ125 <51464325+EJ125@users.noreply.github.com>
2023-12-14 02:19:36 -0800

    Delete external/gcn_c/build directory

--- external/gcn_c/build/gu.d
+++
@@ -1,4 +0,0 @@
-gu.o: \
- C:/Users/corra/Downloads/tww-gz-any_saves/tww-gz-any_saves/external/gcn_c/src/gu.c \
- C:/Users/corra/Downloads/tww-gz-any_saves/tww-gz-any_saves/external/gcn_c/include/gu.h
-C:/Users/corra/Downloads/tww-gz-any_saves/tww-gz-any_saves/external/gcn_c/include/gu.h:
