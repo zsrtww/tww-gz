0c5fa6df43be423c7ade278386a340a7153a49a6
EJ125 <51464325+EJ125@users.noreply.github.com>
2023-12-14 23:57:28 -0800

    Update save_specials.cpp

--- source/save_specials.cpp
+++ source/save_specials.cpp
@@ -51,15 +51,34 @@ void SaveMngSpecial_Hyrule2_BarrierSkip() {
     g_dComIfG_gameInfo.play.mNextStage.setLayer(2);
 }
 
+void SaveMngSpecial_Hyrule2_BarrierSkip_After() {
+    gSaveManager.injectDefault_after();
+    g_dComIfG_gameInfo.info.getPlayer().getPlayerStatusA().setLife(6);
+    g_dComIfG_gameInfo.info.getPlayer().getItemRecord().setBombNum(19);
+}
+
+
 void SaveMngSpecial_TrialsSkip() {
     gSaveManager.injectDefault_during();
     g_dComIfG_gameInfo.play.mNextStage.setRoomNo(1);
+}
+
+void SaveMngSpecial_TrialsSkip_After() {
+    gSaveManager.injectDefault_after();
+    g_dComIfG_gameInfo.info.getPlayer().getPlayerStatusA().setLife(6);
+    g_dComIfG_gameInfo.info.getPlayer().getItemRecord().setBombNum(17);
 }
 
 void SaveMngSpecial_PuppetGanon() {
     gSaveManager.injectDefault_during();
     g_dComIfG_gameInfo.play.mNextStage.setLayer(1);
     l_equipped_arrow_type = 3;
+}
+
+void SaveMngSpecial_PuppetGanon_After() {
+    gSaveManager.injectDefault_after();
+    g_dComIfG_gameInfo.info.getPlayer().getPlayerStatusA().setLife(8);
+    g_dComIfG_gameInfo.info.getPlayer().getItemRecord().setBombNum(17);
 }
 
 void SaveMngSpecial_MorthHover_during() {
@@ -72,9 +91,60 @@ void SaveMngSpecial_MorthHover_during() {
 void SaveMngSpecial_MorthHover_after() {
     gSaveManager.injectDefault_after();
     g_dComIfG_gameInfo.info.getPlayer().getPlayerStatusA().setLife(1);
+    g_dComIfG_gameInfo.info.getPlayer().getPlayerStatusA().setMagic(8);
+    g_dComIfG_gameInfo.info.getPlayer().getItemRecord().setBombNum(12);
 }
 
 void SaveMngSpecial_Ganondorf() {
     gSaveManager.injectDefault_during();
     g_dComIfG_gameInfo.play.mNextStage.setLayer(1);
 }
+
+void SaveMngSpecial_Ganondorf_After() {
+    gSaveManager.injectDefault_after();
+    g_dComIfG_gameInfo.info.getPlayer().getPlayerStatusA().setLife(4);
+    g_dComIfG_gameInfo.info.getPlayer().getItemRecord().setBombNum(10);
+}
+
+void SaveMngSpecial_LeafHover() {
+    gSaveManager.injectDefault_during();
+    g_dComIfG_gameInfo.play.mNextStage.setPoint(6);
+}
+
+void SaveMngSpecial_LeafHover_After() {
+    gSaveManager.injectDefault_after();
+    g_dComIfG_gameInfo.info.getPlayer().getItemRecord().setBombNum(20);
+    g_dComIfG_gameInfo.info.getPlayer().getPlayerStatusA().setLife(1);
+}
+
+void SaveMngSpecial_QuiverAny_After() {
+    gSaveManager.injectDefault_after();
+    g_dComIfG_gameInfo.info.getPlayer().getItemRecord().setBombNum(19);
+}
+
+void SaveMngSpecial_LightArrowSkip_After() {
+    gSaveManager.injectDefault_after();
+    g_dComIfG_gameInfo.info.getPlayer().getItemRecord().setBombNum(17);
+    g_dComIfG_gameInfo.info.getPlayer().getPlayerStatusA().setLife(6);
+    g_dComIfG_gameInfo.info.getPlayer().getPlayerStatusA().setMagic(6);
+}
+
+void SaveMngSpecial_PGCutsceneSkip_After() {
+    gSaveManager.injectDefault_after();
+    g_dComIfG_gameInfo.info.getPlayer().getItemRecord().setBombNum(16);
+    g_dComIfG_gameInfo.info.getPlayer().getPlayerStatusA().setLife(6);
+    g_dComIfG_gameInfo.info.getPlayer().getPlayerStatusA().setMagic(10);
+}
+
+void SaveMngSpecial_PGSkip() {
+    gSaveManager.injectDefault_during();
+    g_dComIfG_gameInfo.play.mNextStage.setLayer(8);
+}
+
+void SaveMngSpecial_PGSkip_After() {
+    gSaveManager.injectDefault_after();
+    g_dComIfG_gameInfo.info.getPlayer().getItemRecord().setBombNum(15);
+    g_dComIfG_gameInfo.info.getPlayer().getPlayerStatusA().setLife(1);
+    g_dComIfG_gameInfo.info.getPlayer().getPlayerStatusA().setMagic(13);
+}
+
