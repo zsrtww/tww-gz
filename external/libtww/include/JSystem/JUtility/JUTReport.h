42d495af46151db0cb98fc850f9f4b2efd478d0c
fig02 <fig02srl@gmail.com>
2024-08-25 04:43:14 -0400

    customize map select

---
+++ external/libtww/include/JSystem/JUtility/JUTReport.h
@@ -0,0 +1,9 @@
+#ifndef JSYSTEM_JUTILITY_JUTREPORT_H
+#define JSYSTEM_JUTILITY_JUTREPORT_H
+
+#include "defines.h"
+
+LIBTWW_DEFINE_FUNC(JUTReport__FiiPCce, void, JUTReport, (int, int, const char*, ...))
+
+
+#endif
